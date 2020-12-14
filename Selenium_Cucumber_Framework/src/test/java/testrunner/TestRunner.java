24bd91abd87aab083ae01cbb0a5e1b8bf8638fee
cscoder2k <cscoder2k@gmail.com>
2020-12-14 09:10:39 +0530

    testrunner

--- Selenium_Cucumber_Framework/src/test/java/testrunner/TestRunner.java
+++ Selenium_Cucumber_Framework/src/test/java/testrunner/TestRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.*;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features",glue= {"src/test/java/stepdefinitions"})
+@CucumberOptions(features="Features/",glue= {"src/test/java/stepdefinitions"})
 public class TestRunner {
 
 }
