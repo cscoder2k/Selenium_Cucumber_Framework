63b931420f2892969d2d35f71d4a35665b5465e8
cscoder2k <cscoder2k@gmail.com>
2020-12-14 09:36:23 +0530

    added step definition

--- Selenium_Cucumber_Framework/src/test/java/stepdefinitions/SD0001_SampleStepDefinition.java
+++ Selenium_Cucumber_Framework/src/test/java/stepdefinitions/SD0001_SampleStepDefinition.java
@@ -1,5 +1,35 @@
 package stepdefinitions;
 
-public class SD0001_SampleStepDefinition {
+import io.cucumber.java.en.*;
 
+public class SD0001_SampleStepDefinition {
+	@Given("Application URL should be launched in browser")
+	public void application_url_should_be_launched_in_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter xxxx")
+	public void i_enter_xxxx() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter yyyy")
+	public void i_enter_yyyy() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on Login")
+	public void i_click_on_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify whether login is successful.")
+	public void i_verify_whether_login_is_successful() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
